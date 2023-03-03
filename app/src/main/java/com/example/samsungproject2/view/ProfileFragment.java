f36e069f96a450a5523420b323e8c337aa161c68
gavrilovds <dimagavrilov2004@yandex.ru>
2023-03-03 21:25:15 +0300

    Added animation to reg/log screen

--- app/src/main/java/com/example/samsungproject2/view/ProfileFragment.java
+++ app/src/main/java/com/example/samsungproject2/view/ProfileFragment.java
@@ -2,25 +2,76 @@ package com.example.samsungproject2.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.transition.Transition;
+import androidx.transition.TransitionInflater;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.samsungproject2.R;
 import com.example.samsungproject2.databinding.FragmentMapBinding;
 import com.example.samsungproject2.databinding.FragmentProfileBinding;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private Button signUp;
+    private Button logIn;
+    private ImageView cocktailImg;
+    private TextView welcomeText;
+    private TextView welcomeText2;
+    private TextInputLayout emailInput;
+    private TextInputLayout passwordInput;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO ПОРАБОТАТЬ НАД АНИМАЦИЕЙ "УЖЕ ЕСТЬ АККАУНТ"
+        //Transition transition = TransitionInflater.from(requireContext())
+        //        .inflateTransition(R.transition.shared_views);
+        //int cnt = getActivity().getSupportFragmentManager().getBackStackEntryCount();
+        // if (cnt!=0){
+        //  if (String.valueOf(getActivity().getSupportFragmentManager().getBackStackEntryAt(cnt-1).getName())=="from_reg_fragment")
+        //      setSharedElementEnterTransition(transition);
+        //}
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
+        signUp = binding.signInButton;
+        logIn = binding.logInButton;
+        cocktailImg = binding.cocktailImg;
+        welcomeText = binding.welcomeText;
+        welcomeText2 = binding.welcomeText2;
+        emailInput = binding.emailInput;
+        passwordInput = binding.passwordInput;
 
+        signUp.setOnClickListener(v -> getActivity().getSupportFragmentManager()
+                .beginTransaction()
+                .addSharedElement(signUp, "have_an_account_transition")
+                .addSharedElement(logIn, "reg_log_transition")
+                .addSharedElement(cocktailImg, "cocktail_transition")
+                .addSharedElement(welcomeText, "text_transition")
+                .addSharedElement(welcomeText2, "text2_transition")
+                .addSharedElement(emailInput, "email_transition")
+                .addSharedElement(passwordInput, "password_transition")
+                .replace(R.id.body_container, new RegistrationFragment())
+                .addToBackStack(null)
+                .commit());
 
 
         return binding.getRoot();
---
+++ app/src/main/java/com/example/samsungproject2/view/RegistrationFragment.java
@@ -0,0 +1,74 @@
+package com.example.samsungproject2.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.transition.Transition;
+import androidx.transition.TransitionInflater;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.samsungproject2.R;
+import com.example.samsungproject2.databinding.FragmentRegistrationBinding;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+public class RegistrationFragment extends Fragment {
+
+    private FragmentRegistrationBinding binding;
+    private Button alrdHaveAnAcc;
+    private TextView text1;
+    private TextView text2;
+    private TextInputLayout emailInput;
+    private TextInputLayout passwordInput;
+    private Button registerButton;
+    private ImageView cocktailImg;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Transition transition = TransitionInflater.from(requireContext())
+                .inflateTransition(R.transition.shared_views);
+        setSharedElementEnterTransition(transition);
+        setSharedElementReturnTransition(transition);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentRegistrationBinding.inflate(inflater, container, false);
+        alrdHaveAnAcc = binding.haveAnAccountButton;
+        text1 = binding.welcomeText;
+        text2 = binding.welcomeText2;
+        emailInput = binding.emailInput;
+        passwordInput = binding.passwordInput;
+        registerButton = binding.registerButton;
+        cocktailImg = binding.cocktailImg;
+
+        alrdHaveAnAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity()
+                        .getSupportFragmentManager()
+                        .beginTransaction()
+                        .addSharedElement(alrdHaveAnAcc, "have_an_account_transition")
+                        .addSharedElement(text1, "text_transition")
+                        .addSharedElement(text2, "text2_transition")
+                        .addSharedElement(emailInput, "email_transition")
+                        .addSharedElement(passwordInput, "password_transition")
+                        .addSharedElement(registerButton,"reg_log_transition")
+                        .addSharedElement(cocktailImg, "cocktail_transition")
+                        .replace(R.id.body_container, new ProfileFragment())
+                        .addToBackStack("from_reg_fragment")
+                        .commit();
+            }
+        });
+
+        return binding.getRoot();
+    }
+}
