20f48b53edbf27cefdab223b4cff5f386191636e
gavrilovds <dimagavrilov2004@yandex.ru>
2023-03-05 15:21:23 +0300

    Added UserProfileFragment

---
+++ app/src/main/java/com/example/samsungproject2/view/UserProfileFragment.java
@@ -0,0 +1,25 @@
+package com.example.samsungproject2.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.samsungproject2.R;
+import com.example.samsungproject2.databinding.FragmentUserProfileBinding;
+
+
+public class UserProfileFragment extends Fragment {
+
+    private FragmentUserProfileBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        binding = FragmentUserProfileBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+}
